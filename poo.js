6cd11e67188502dd08cd4eab87f81da2987e7fe0
annaddssdd <annaddssdd@gmail.com>
2019-07-25 12:06:09 +1200

    Update poo.js

--- poo.js
+++ poo.js
@@ -1,12 +1,18 @@
 function myFunction() {
 document.getElementById("graphics").style.display = 'block';
+document.getElementById("graphicCont").style.border = '3px solid yellow';
+document.getElementById("animationsCont").style.border = '0';
 document.getElementById("animations").style.display = 'none';
 }
 
 function myFunction2() {
 document.getElementById("animations").style.display = 'block';
+document.getElementById("animationsCont").style.border = '3px solid yellow';
+document.getElementById("graphicCont").style.border = '0';
 document.getElementById("graphics").style.display = 'none';
+
 }
+
 
 
 window.onscroll = function() {scrollFunction()};
